7589163502956ba44aa104d1ebce65adc2b5d261
Hellen Fiallos <hellen.fiallos@agilesolutions.com>
2017-06-21 11:26:25 -0600

    added database connection

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -16,4 +16,9 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.row_factory
+    return rv
 # The Config object works similarly to a dictionary, so it can be updated with new values
